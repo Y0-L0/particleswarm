e08b0ded69238de187332b024c7184f392c41bb4
schoersch <14273062+schoersch@users.noreply.github.com>
2025-12-30 12:54:12 +0100

    + and += for Vec3D

--- src/math/particle.rs
+++ src/math/particle.rs
@@ -34,4 +34,32 @@ impl Particle {
             acceleration,
         }
     }
+
+    fn update(&mut self) {
+        // TODO cont
+        // self.velocity += self.acceleration;
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use rand::{SeedableRng, rngs::StdRng};
+
+    use super::*;
+
+    #[test]
+    fn spawn() {
+        let mut rng = StdRng::seed_from_u64(1);
+        let particle = Particle::spawn(&mut rng);
+
+        assert_ne!(0.0, particle.acceleration.x);
+        assert_ne!(0.0, particle.acceleration.y);
+        assert_ne!(0.0, particle.acceleration.z);
+        assert_ne!(0.0, particle.position.x);
+        assert_ne!(0.0, particle.position.y);
+        assert_ne!(0.0, particle.position.z);
+        assert_ne!(0.0, particle.velocity.x);
+        assert_ne!(0.0, particle.velocity.y);
+        assert_ne!(0.0, particle.velocity.z);
+    }
 }
--- src/math/vec.rs
+++ src/math/vec.rs
@@ -1,3 +1,5 @@
+use std::ops;
+
 #[derive(Debug, Clone, PartialEq)]
 pub struct Vec3D {
     pub x: f64,
@@ -11,8 +13,30 @@ impl Vec3D {
     }
 }
 
+impl ops::Add<Vec3D> for Vec3D {
+    type Output = Vec3D;
+
+    fn add(self, rhs: Vec3D) -> Vec3D {
+        Vec3D {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl ops::AddAssign for Vec3D {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+        self.z += rhs.z;
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use core::f64;
+
     use super::*;
     use approx::assert_relative_eq;
     use test_case::test_case;
@@ -24,5 +48,41 @@ mod tests {
     fn test_norm1_int(x: f64, y: f64, z: f64, expected: f64) {
         let vec = Vec3D { x, y, z };
         assert_relative_eq!(expected, vec.norm1(), epsilon = f64::EPSILON);
+    }
+
+    #[test]
+    fn test_add_vec3d() {
+        let vec1 = Vec3D {
+            x: 1.0,
+            y: 2.0,
+            z: 3.0,
+        };
+        let vec2 = Vec3D {
+            x: 4.0,
+            y: 5.0,
+            z: 6.0,
+        };
+        let vec_sum = vec1 + vec2;
+        assert_relative_eq!(5.0, vec_sum.x, epsilon = f64::EPSILON);
+        assert_relative_eq!(7.0, vec_sum.y, epsilon = f64::EPSILON);
+        assert_relative_eq!(9.0, vec_sum.z, epsilon = f64::EPSILON);
+    }
+
+    #[test]
+    fn test_add_assign_vec3d() {
+        let mut vec1 = Vec3D {
+            x: 1.0,
+            y: 2.0,
+            z: 3.0,
+        };
+        let vec2 = Vec3D {
+            x: 4.0,
+            y: 5.0,
+            z: 6.0,
+        };
+        vec1 += vec2;
+        assert_relative_eq!(5.0, vec1.x, epsilon = f64::EPSILON);
+        assert_relative_eq!(7.0, vec1.y, epsilon = f64::EPSILON);
+        assert_relative_eq!(9.0, vec1.z, epsilon = f64::EPSILON);
     }
 }
